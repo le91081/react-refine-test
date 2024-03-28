1f2ade33a032c2cf360c39373bedd089a8b30861
Bert <le91081@gmail.com>
2024-03-28 22:07:17 +0800

    update: App routes and layout setup

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Row, Col } from "antd";
 
 export const Home = () => {
   return (
     <div>
-      
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          Calendar
+        </Col>
+        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+          Dashboard
+        </Col>
+      </Row>
     </div>
-  )
-}
-
+  );
+};
